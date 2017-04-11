742c71e213d7e3f8e581def02ae2d219ba55af27
techsoft83 <techsoft83@gmail.com>
2017-04-11 10:54:19 +0530

    mvc

---
+++ 8march/ERPOLD/ERPOLD/Models/ViewModel/SaleRegisterVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERPOLD.Models.ViewModel
+{
+    public class SaleRegisterVM
+    {
+        public string ACCOUNTNAME { get; set; }
+        public DateTime BILLDATE { get; set; }
+        public string BILLNO { get; set; }
+        public string PAYMENTMODE { get; set; }
+        public Nullable<decimal> BILLAMOUNT { get; set; }
+    }
+}
---
+++ 8march/ERPOLD/ERPOLD/Models/ViewModel/Stockreport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERPOLD.Models.ViewModel
+{
+    public class Stockreport
+    {
+        public Nullable<int> ITEMID { get; set; }
+        public Nullable<int> SIZEID { get; set; }
+        public Nullable<System.DateTime> DateFrom { get; set; }
+        public Nullable<System.DateTime> DateTo { get; set; }
+    }
+}
